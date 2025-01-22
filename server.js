5d4949a3d824ea254f4ca2291a232b398ff08cd7
Parwinder <kauraparwinder@gmail.com>
2025-01-22 21:08:51 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+
+app.get('/', (req, res) => {
+    res.send("Welcome to the API");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
